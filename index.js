98f0c2437667a049d43cd3e8bd408a2cfbbc85cb
Vũ Quang Thịnh <thinh@agiletech.vn>
2018-12-10 15:14:44 +0700

    updatecode

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-import objectPath from 'object-path';
-import locales from 'app/locales';
+const objectPath = require('object-path');
 
 /**
  * @param {Function} detect detect = (req, res, next) => { return req.headers['x-locale'] }
  */
-export default ({ detect, locales }) => (req, res, next) => {
+module.exports = ({ detect, locales }) => (req, res, next) => {
   const locale = detect(req, res, next);
   //  req.headers['x-locale'] || 'en';
   req.locale = locale;
 
   req.t = (code, params = {}) => {
-    const text = objectPath.get(locales[locale], code, code);
+    let text = objectPath.get(locales[locale], code, code);
 
     if (params) {
-      for (var k in params) {
+      for (let k in params) {
         if (typeof params[k] !== 'function') {
           text = text.replace('{{{' + k + '}}}', params[k].toString());
         }
